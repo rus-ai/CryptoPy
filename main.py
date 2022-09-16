1f7614710653252d4605a0d118cb530510b77882
rus-ai <russian-ai@yandex.ru>
2022-09-16 21:34:59 +0300

    add asn1 decoder

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from classes.CurrentUser import CurrentUser
 from classes.CryptoProRegistry import CryptoProRegistry
+from modules.AsnDecoder import print_keyfile
 
 
 def main():
@@ -8,9 +9,10 @@ def main():
     print(f"Current user", user)
     print(f"CryptoPro CSP path", cryptopro.keypath)
     print("Container list:")
-    for container in cryptopro.containers():
-        print(container)
-        print(cryptopro.get_container(container))
+    for container_name in cryptopro.containers():
+        print(container_name)
+        container = cryptopro.get_container(container_name)
+        print_keyfile(container.key_name)
 
 
 if __name__ == '__main__':
